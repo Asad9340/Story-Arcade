ce95c58e210f7ed3eaae761694dbb64d41dd5ca2
Asad9340 <asadulimran1999@gmail.com>
2024-03-22 02:21:02 +0600

    added button and hero section responsive for mobile device

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,23 @@
-
+import { NavLink } from "react-router-dom";
 function Hero() {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-200 mt-10">
       <div className="hero-content text-center">
         <div>
-          <h1 className="text-5xl font-bold">Welcome to StoryArcade </h1>
+          <h1 className="text-2xl md:text-5xl font-bold ">
+            Welcome to StoryArcade{' '}
+          </h1>
           <p className="py-6 max-w-2xl">
             StoryArcade is the bridge between the complex world of technology
             and the curious minds eager to understand it
           </p>
           <div className="flex gap-4 items-center justify-center">
-            <a
-              href="#"
-              className="relative inline-flex items-center justify-center p-4 px-12 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
+            <NavLink to='/blogs'
+              className="relative inline-flex items-center justify-center p-5 md:p-4 px-4 md:px-12 py-2 md:py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
             >
               <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
                 <svg
-                  className="w-6 h-6"
+                  className="w-5 h-5"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -34,15 +35,15 @@ function Hero() {
                 Read Blogs
               </span>
               <span className="relative invisible">Button Text</span>
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50"
+            </NavLink>
+            <NavLink
+              to='/bookmarks'
+              className="relative inline-flex items-center  px-4 md:px-8 lg:px-12 py-2 md:py-3 overflow-hidden text-base font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50"
             >
               <span className="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
               <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
                 <svg
-                  className="w-5 h-5"
+                  className="w-5 h-5 hidden md:block"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -57,7 +58,7 @@ function Hero() {
                 </svg>
               </span>
               <span className="relative">Button Text</span>
-            </a>
+            </NavLink>
           </div>
         </div>
       </div>
